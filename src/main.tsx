18481f51e3372545242336e6752c89f704fe0d6e
Abdulkareem771 <wowkareem@gmail.com>
2026-07-03 17:57:05 +0300

    fix: switch to HashRouter to prevent blank screen on GitHub Pages

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { App } from "./App";
 import "./styles.css";
 
@@ -12,8 +12,8 @@ if ("serviceWorker" in navigator && import.meta.env.PROD) {
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter basename="/Personal_Rehabilitation_Companion">
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
